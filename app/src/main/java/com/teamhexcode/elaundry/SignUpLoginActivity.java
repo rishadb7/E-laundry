35f88c9008f329cb86a33538509c29ba924c5a4c
rishadb7 <rishadb7@gmail.com>
2018-02-25 11:41:46 +0530

    first commit

---
+++ app/src/main/java/com/teamhexcode/elaundry/SignUpLoginActivity.java
@@ -0,0 +1,13 @@
+package com.teamhexcode.elaundry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_login);
+    }
+}
